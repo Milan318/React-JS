6c13a8f1f44c0b51124e0b990d1d2f5bd74c01bf
Milan318 <milanmandaviya7495@gmail.com>
2025-04-22 16:54:53 +0530

    done

--- array-object/index.js
+++ array-object/index.js
@@ -1,11 +1,28 @@
 // OBJECT
 
+// let obj = {
+//     name:"milan",
+//     age:24,
+//     print:function(){
+//        console.log(`hello ${this.name}`);
+//     }
+// };
+// obj.print();
+
+// destructuring object
+
 let obj = {
     name:"milan",
     age:24,
-    print:function(){
-       console.log(`hello ${this.name}`);
-    }
-};
-obj.print();
+    password:12345
+}
+
+const {name,age,password} = obj;
+console.log(name);
+console.log(age);
+console.log(password);
+
+
+
+
 
