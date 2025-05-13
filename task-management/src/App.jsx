db63665a305df910e7c2e5c99fbe32edab4cef12
Milan318 <milanmandaviya7495@gmail.com>
2025-05-13 16:57:25 +0530

    done

---
+++ task-management/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 mt-3 mx-auto">
+            <h2 className='text-center'>TASK MANAGEMENT</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
