2dc380bb9c0ab285a24203216aad92c6f4122d76
Milan318 <milanmandaviya7495@gmail.com>
2025-05-16 09:06:50 +0530

    done

--- form-revision/src/App.jsx
+++ form-revision/src/App.jsx
@@ -4,22 +4,9 @@ function App() {
    
      const [employee, setEmployee] = useState({});
      const [empData, setEmpData] = useState([]);
-     const [count, setCount] = useState([]);
 
      const handleChange = (e)=>{
-      const {name,value,checked}= e.target
-
-      if(name === "count"){
-        let newCount = [...count]
-        if(checked){
-          newCount.push(value)
-        }
-        else{
-          newCount = newCount.filter((item)=> item != value)
-        }
-        setCount(newCount)
-      }
-
+      const {name,value}= e.target
       let data = {...employee,[name]:value};
       setEmployee(data);
      }
@@ -28,19 +15,7 @@ function App() {
       e.preventDefault();
       let data = [...empData,{...employee,id:Date.now()}];
       setEmpData(data);
-      setEmployee({});
-      setCount([]);
      }
-
-     const handleDelete = (id)=>{
-      let data = empData.filter(val => val.id !== id)
-      setEmpData(data);
-    }
-
-    const handleEdit = (id)=>{
-      let data = empData.filter((val,idx)=>val.id === id)[0]
-      setEmployee(data);
-    }
 
   return (
     <>
@@ -51,11 +26,11 @@ function App() {
           <h2 className='text-center'>EMPLOYEE DATA</h2>
                 <div className="mb-3">
                   <label htmlFor="ename" className="form-label">Employee Name</label>
-                  <input type="text" className="form-control" id="ename" name='ename' onChange={handleChange} value={employee.ename || ""}/>
+                  <input type="text" className="form-control" id="ename" name='ename' onChange={handleChange}/>
                 </div>
                 <div className="mb-3">
                   <label htmlFor="salary" className="form-label">Salary</label>
-                  <input type="text" className="form-control" id="salary" name='salary' onChange={handleChange} value={employee.salary || ""}/>
+                  <input type="text" className="form-control" id="salary" name='salary' onChange={handleChange}/>
                 </div>
                 <div className="mb-3 form-check-inline">
                   <input type="checkbox" className="form-check-input" id="count" />
@@ -91,16 +66,16 @@ function App() {
               <tbody>
                    {
                     empData.map((value,index)=>{
-                      const {ename,salary,id}= value
+                      const {ename,salary}= value
                       return(
                         <tr key={index}>
                           <td>{index+1}</td>
                           <td>{ename}</td>
                           <td>{salary}</td>
-                          <td>{val.count.toString()}</td>
+                          <td></td>
                           <td>
-                            <button onClick={()=>handleDelete(id)}  className='btn btn-danger'>Delete</button>{" "}
-                            <button onClick={()=>handleEdit(id)} className='btn btn-warning'>Edit</button>
+                            <button className='btn btn-danger'>Delete</button>{" "}
+                            <button className='btn btn-warning'>Edit</button>
                           </td>
                         </tr>
                       );
