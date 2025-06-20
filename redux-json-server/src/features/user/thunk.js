4d86af0711b2d51b5ed7e453f87df73a6fdb8de1
Milan318 <milanmandaviya7495@gmail.com>
2025-06-20 16:31:49 +0530

    done

---
+++ redux-json-server/src/features/user/thunk.js
@@ -0,0 +1,23 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios"
+
+export const fetch = createAsyncThunk('fetch/product',async()=>{
+    let res = await axios.get("http://localhost:3000/product");
+    return res.data
+})
+
+export const createProduct = createAsyncThunk('create/product',async(product)=>{
+    let res = await axios.post("http://localhost:3000/product",product);
+    return res.data;
+})
+
+export const updateProduct = createAsyncThunk('updateProduct',async(product)=>{
+    let res = await axios.put(`"http://localhost:3000/product"/${product.id}`,product);
+    return res.data;
+})
+
+export const deleteproduct = createAsyncThunk('delete/product',async(id)=>{
+    let res = await axios.delete(`http://localhost:3000/product/${product.id}`);
+    return id;
+})
+
---
+++ redux-json-server/src/features/user/userSlice.js
@@ -0,0 +1,22 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const initialState ={
+    list:[],
+}
+
+
+
+export const productSlice = createSlice({
+    name:'product',
+    initialState,
+    reducers:{
+
+    },
+    extraReducers:(builder)=>{
+
+    }
+
+    
+})
+
+export default productSlice.reducer
