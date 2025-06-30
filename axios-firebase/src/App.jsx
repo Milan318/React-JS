ea3f87809c48ca5ea5a446452124d15a1a815a83
Milan318 <milanmandaviya7495@gmail.com>
2025-06-30 16:30:35 +0530

    done

---
+++ axios-firebase/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Form from './components/From'
+import Table from './components/Table'
+
+const App = () => {
+  return (
+    <>
+    <Form/>
+    <Table/>
+      
+    </>
+  )
+}
+
+export default App
